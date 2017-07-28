af3c0f396481a33a323d18560394ec4ffa8cf9f6
Ma.Belen <mguarand@espol.edu.ec>
2017-07-27 22:55:14 -0500

    clase formulario, modelo y html para crear una factura

---
+++ practica_mvc/forms.py
@@ -0,0 +1,21 @@
+
+from django import forms
+from .models import Factura
+
+import datetime
+
+# Create your models here.
+IMP_CHOICES = (
+    ('0', 'pagado'),
+    ('1', 'pendiente'),
+    ('2', 'acumulado'),
+
+)
+class FacturaForm(forms..ModelForm):
+    numero_factura = forms.CharField(max_length=16)
+    nombre_empresa = forms.CharField(max_length=50)
+    fecha_pago = forms.DateField(initial=datetime.date.today,input_formats=['%d/%m/%Y'])
+    cantidad = forms.DecimalField(min_value=0, decimal_places=2)
+ 
+    estado = forms.ChoiceField(choices=IMP_CHOICES)
+
