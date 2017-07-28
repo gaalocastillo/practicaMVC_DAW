7f63c07dcd1761d3a6b6af2ecc43262431d2d68e
Ma.Belen <mguarand@espol.edu.ec>
2017-07-28 08:42:37 -0500

    se puede agregar una factura con opciones en el campo estado

--- practica_mvc/models.py
+++ practica_mvc/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 import datetime
 
+IMP_CHOICES = (
+    ('0', 'Pagado'),
+    ('1', 'Pendiente'),
+    ('2', 'Acumulado'),
+
+)
 class Factura(models.Model):
     numero_factura = models.CharField('Num factura', primary_key=True,max_length=16)
     empresa = models.CharField(default="-",max_length=200)
     fecha_pago = models.DateField(default=datetime.date.today)
     cantidad = models.DecimalField(default=0,max_digits=6,decimal_places=2)
-    estado = models.CharField(default="acumulado",max_length=30)
+    estado = models.CharField(default="acumulado",max_length=25,choices=IMP_CHOICES)
 
 
     def guardar(self):
