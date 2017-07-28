7163c1113fd4275c01932b6b28304699c4fa1677
Jonathan <jgorotiz@espol.edu.ec>
2017-07-28 02:16:39 -0500

    U<RL agregada

--- practica_mvc/urls.py
+++ practica_mvc/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
         url(r'^$', views.get_name),
         url(r'^thanks/', views.show_name),
+        url(r'^listar/', views.listar_facturas),
         # url(r'^facturas/lista/', views.listar_facturas),
         # url(r'^facturas/(\d+)/$', views.actualizar_factura),
 
